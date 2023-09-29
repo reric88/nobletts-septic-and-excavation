03aa75f1dc32ef8d733bed5ef2707bda7d45c8f4
Eric Collard <rcollard88@gmail.com>
2023-09-29 19:33:48 -0400

    Additional navbar styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,9 +3,7 @@ import React from 'react'
 export const Home = () => {
   return (
     <div className='home'>
-    <header>
-          <h1>Noblett’s Septic & Excavating</h1>
-        </header>
+   
         
     </div>
   )
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -18,11 +18,17 @@ const slideBladesUp = () => {
 
   return (
     <div className="full-container">
-        <button onClick={props.setButtonClicked} className='block-button'></button>
         <div className='navigation'>
             <div className={`block block1 ${props.testClicked ? 'block1-down' : 'block1-up'}`}></div>
             <div className={`block block2 ${props.testClicked ? 'block2-down' : 'block2-up'}`}></div>
             <div className={`block block3 ${props.testClicked ? 'block3-down' : 'block3-up'}`}></div>
+            
+            <nav className='navbar'>
+            <header>
+            <button onClick={props.setButtonClicked} className='block-button'></button>
+          <h1>Noblett’s Septic & Excavating</h1>
+        </header>
+            </nav>
         </div>
     </div>
     
