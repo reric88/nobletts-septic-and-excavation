99ee910186e263397112430d2b9a66ac4fc5c04f
Eric Collard <rcollard88@gmail.com>
2023-10-02 22:36:38 -0400

    styled nav-menu and its list in Navigation component. Changed header h1 and h2 to allow increase in size in Navigation component.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -22,12 +22,28 @@ const slideBladesUp = () => {
             <div className={`block block1 ${props.testClicked ? 'block1-down' : 'block1-up'}`}></div>
             <div className={`block block2 ${props.testClicked ? 'block2-down' : 'block2-up'}`}></div>
             <div className={`block block3 ${props.testClicked ? 'block3-down' : 'block3-up'}`}></div>
+            <div className={`block block4 ${props.testClicked ? 'block4-down' : 'block4-up'}`}></div>
             
             <nav className='navbar'>
             <header className={`${!props.testClicked ? 'header-border' : ''}`}>
             <button onClick={props.setButtonClicked} className='block-button'><i className="fa-solid fa-bars"></i></button>
-          <h1>Noblett’s Septic & Excavating</h1>
+          <h1>Noblett’s</h1>
+          <h2>
+            <span className='sp-1'>S</span> 
+            <span>&</span> 
+            <span className='sp-2'>E</span>
+            </h2>
+
         </header>
+        <div className={`nav-menu ${props.testClicked ? 'nav-menu-down' : 'nav-menu-up'}`}>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#creator">Developer</a></li>
+          </ul>
+        </div>
             </nav>
         </div>
     </div>
